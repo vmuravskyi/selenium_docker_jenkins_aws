dc01f6e74616f15ee98f421d005192eec07610a2
v.muravskyi <v.muravskyi@gmail.com>
2024-02-25 13:46:34 +0100

    imports

--- src/test/java/flightreservationtest/FlightReservationTest.java
+++ src/test/java/flightreservationtest/FlightReservationTest.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
