7f6452b9fd5605a9b91271727964804a238243c7
v.muravskyi <v.muravskyi@gmail.com>
2024-03-02 00:57:33 +0100

    update with ability to run in containers with remote driver

--- src/test/java/com/automation/framework/tests/BaseTest.java
+++ src/test/java/com/automation/framework/tests/BaseTest.java
@@ -1,14 +1,18 @@
 package com.automation.framework.tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,24 +21,34 @@ public abstract class BaseTest {
 	protected WebDriver driver;
 
 	@BeforeTest
-	@Parameters("headless")
-	public void setup(Boolean headless) {
-		WebDriverManager.chromedriver().setup();
-		if (headless) {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("headless");
-			this.driver = new ChromeDriver(options);
+	public void setDriver() throws MalformedURLException {
+		if (Boolean.valueOf(System.getProperty("selenium.grid.enabled"))) {
+			this.driver = getRemoteDriver();
 		} else {
-			this.driver = new ChromeDriver();
+			this.driver = getLocalDriver();
 		}
 		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		this.driver.manage().window().setSize(new Dimension(1920, 1080));
 	}
 
-	@AfterTest
-	public void tearDown() {
-		this.driver.close();
+	@AfterTest(alwaysRun = true)
+	public void quitDriver() {
 		this.driver.quit();
+	}
+
+	private WebDriver getLocalDriver() {
+		WebDriverManager.chromedriver().setup();
+		return new ChromeDriver();
+	}
+
+	private WebDriver getRemoteDriver() throws MalformedURLException {
+		Capabilities capabilities;
+		if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+			capabilities = new ChromeOptions();
+		} else {
+			capabilities = new FirefoxOptions();
+		}
+		return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
 	}
 
 }
