7d8905d4de4e1defa8ff29ff497f874401f699e5
v.muravskyi <v.muravskyi@gmail.com>
2024-12-07 23:23:23 +0100

    updates, rename main test suite

--- src/test/java/com/automation/framework/tests/BaseTest.java
+++ src/test/java/com/automation/framework/tests/BaseTest.java
@@ -4,10 +4,13 @@ import static com.automation.framework.utils.Constants.BROWSER;
 import static com.automation.framework.utils.Constants.FIREFOX;
 import static com.automation.framework.utils.Constants.GRID_HUB_HOST;
 
+import com.automation.framework.tests.listener.TestListener;
+import com.automation.framework.utils.Config;
+import com.automation.framework.utils.Constants;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -23,55 +26,49 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 
-import com.automation.framework.tests.listener.TestListener;
-import com.automation.framework.utils.Config;
-import com.automation.framework.utils.Constants;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 @Listeners({TestListener.class})
 public abstract class BaseTest {
 
-	protected static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
-	protected WebDriver driver;
+    protected static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+    protected WebDriver driver;
 
-	@BeforeSuite
-	public void setupConfig() {
-		Config.initialize();
-	}
+    @BeforeSuite
+    public void setupConfig() {
+        Config.initialize();
+    }
 
-	@BeforeTest
-	public void setDriver(ITestContext context) throws MalformedURLException {
-		this.driver = Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)) ? getRemoteDriver() : getLocalDriver();
-		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		this.driver.manage().window().setSize(new Dimension(1920, 1080));
-		context.setAttribute(Constants.DRIVER, this.driver);
-	}
+    @BeforeTest
+    public void setDriver(ITestContext context) throws MalformedURLException {
+        this.driver = Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)) ? getRemoteDriver() : getLocalDriver();
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        this.driver.manage().window().setSize(new Dimension(1920, 1080));
+        context.setAttribute(Constants.DRIVER, this.driver);
+    }
 
-	@AfterTest(alwaysRun = true)
-	public void quitDriver() {
-		this.driver.quit();
-	}
+    @AfterTest(alwaysRun = true)
+    public void quitDriver() {
+        this.driver.quit();
+    }
 
-	private WebDriver getLocalDriver() {
-		WebDriverManager.chromedriver().setup();
-		return new ChromeDriver();
-	}
+    private WebDriver getLocalDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
 
-	private WebDriver getRemoteDriver() throws MalformedURLException {
-		Capabilities capabilities;
-		if (Config.get(BROWSER).equalsIgnoreCase(FIREFOX)) {
-			capabilities = new FirefoxOptions();
-		} else {
-			capabilities = new ChromeOptions();
-		}
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+        if (Config.get(BROWSER).equalsIgnoreCase(FIREFOX)) {
+            capabilities = new FirefoxOptions();
+        } else {
+            capabilities = new ChromeOptions();
+        }
 
-		// build selenium grid hub url
-		String urlFormat = Config.get(Constants.GRID_URL_FORMAT);
-		String hubHost = Config.get(GRID_HUB_HOST);
-		String url = String.format(urlFormat, hubHost);
-		LOGGER.info("Selenium grid URL: {}", url);
-		return new RemoteWebDriver(new URL(url), capabilities);
-	}
+        // build selenium grid hub url
+        String urlFormat = Config.get(Constants.GRID_URL_FORMAT);
+        String hubHost = Config.get(GRID_HUB_HOST);
+        String url = String.format(urlFormat, hubHost);
+        LOGGER.info("Selenium grid URL: {}", url);
+        return new RemoteWebDriver(new URL(url), capabilities);
+    }
 
 }
